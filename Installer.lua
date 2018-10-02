d5729003a69e09baca637023c1f804203bccbf3b
Sebastian Erik Bauer <dataisloading@outlook.com>
2018-10-02 23:21:44 +0200

    Create Installer.lua

---
+++ Installer.lua
@@ -0,0 +1,23 @@
+print("Installing..")
+local HttpService = game:GetService("HttpService")
+local Module = Instance.new("ModuleScript")
+Module.Name = "AdvancedGroupService"
+Module.Source = HttpService:GetAsync("https://raw.githubusercontent.com/DataIsLoading/AdvancedGroupService/master/AdvancedGroupService.lua")
+Module.Parent = game:GetService("ReplicatedStorage")
+
+local Example = Instance.new("Script")
+Example.Name ="AdvancedGroupService Example"
+Example.Source = [[local GroupService = require(path.to.module)
+local Players = game:GetService("Players")
+local GroupId = 2651565
+
+Players.PlayerAdded:Connect(function(Player) --> DataIsLoading
+  local GroupRank = GroupService:GetRankInGroupAsync(Player.UserId, GroupId)
+  local IsPrimary = GroupService:IsPrimaryGroupAsync(Player.UserId, GroupId)
+  local IsInClan  = GroupService:IsInClanAsync(Player.UserId, GroupId)
+  print("Group Rank:", GroupRank) --> Group Rank: Lead Developer
+  print("Is Primary:", IsPrimary) --> Is Primary: true
+  print("Is In Clan:", IsInClan)  --> Is In Clan: false
+end)]]
+Example.Parent = game:GetService("ServerScriptService")
+print("Installed.")
