57de5797d9526268b20b6b1c3ff3148c39fe6018
howmanysmall <lhamil29@bruinmail.slcc.edu>
2019-01-19 09:27:02 -0700

    YOUR CODE WAS TRASH MATE

--- AdvancedGroupService.lua
+++ AdvancedGroupService.lua
@@ -3,146 +3,132 @@
 	// Author: Sebastian Erik Bauer (DataIsLoading)
 	// Date:   06/01/2019
 --]]
+local GroupService = { }
+local RbxGroupService = game:GetService("GroupService")
 
-local GroupService = game:GetService("GroupService")
-
-local AdvancedGroupService = {}
-local __INTERNAL__ = {}
-	function __INTERNAL__:PagesToArray(pages)
-		local array = {}
-		while true do
-			for key, value in pairs(pages:GetCurrentPage()) do
-				table.insert(array, value)
-			end
-			if (pages.isFinished) then
-				break
-			end
-			pages:AdvanceToNextPageAsync()
-		end
+local function PagesToArray(Pages)
+	local Array = { }
+	while true do
+		for _, Value in next, Pages:GetCurrentPage() do Array[#Array + 1] = Value end
+		if Pages.IsFinished then break end
+		pcall(Pages.AdvanceToNextPageAsync, Pages)
 	end
-
-function AdvancedGroupService:__INTERNALS__() 
-	return __INTERNAL__
 end
 
---[[ AdvancedGroupService Functions ]]--
-function AdvancedGroupService:GetRankInGroupAsync(userId, groupId)
-	local Groups = AdvancedGroupService:GetGroupsAsync(userId)
-	for _,group in pairs(Groups) do
-		if (group.Id == groupId) then
-			return group.Rank
+function GroupService:GetRankInGroupAsync(Player, GroupID)
+	local Groups = GroupService:GetGroupsAsync(Player)
+	for Index = 1, #Groups do
+		local Group = Groups[Index]
+		if Group.Id == GroupID then
+			return Group.Rank
 		end
 	end
 	return 0
 end
 
-function AdvancedGroupService:GetRoleInGroupAsync(userId, groupId)
-	local Groups = AdvancedGroupService:GetGroupsAsync(userId)
-	for _,group in pairs(Groups) do
-		if (group.Id == groupId) then
-			return group.Role
+function GroupService:GetRoleInGroupAsync(Player, GroupID)
+	local Groups = GroupService:GetGroupsAsync(Player)
+	for Index = 1, #Groups do
+		local Group = Groups[Index]
+		if Group.Id == GroupID then
+			return Group.Role
 		end
 	end
 	return "Guest"
 end
 
-function AdvancedGroupService:GetPrimaryGroupAsync(userId)
-	local Groups = AdvancedGroupService:GetGroupsAsync(userId)
-	for _,group in pairs(Groups) do
-		if (group.IsPrimary == true) then
-			return group
+function GroupService:GetPrimaryGroupAsync(Player)
+	local Groups = GroupService:GetGroupsAsync(Player)
+	for Index = 1, #Groups do
+		local Group = Groups[Index]
+		if Group.IsPrimary then
+			return Group
 		end
 	end
 	return nil
 end
 
-function AdvancedGroupService:GetClanAsync(userId)
-	local Groups = AdvancedGroupService:GetGroupsAsync(userId)
-	for _,group in pairs(Groups) do
-		if (group.IsInClan == true) then
-			return group
+function GroupService:GetClanAsync(Player)
+	local Groups = GroupService:GetGroupsAsync(Player)
+	for Index = 1, #Groups do
+		local Group = Groups[Index]
+		if Group.IsInClan then
+			return Group
 		end
 	end
 	return nil
 end
 
-function AdvancedGroupService:IsInGroupAsync(userId, groupId)
-	local Groups = AdvancedGroupService:GetGroupsAsync(userId)
-	for _,group in pairs(Groups) do
-		if (group.Id == groupId) then
+function GroupService:IsInGroupAsync(Player, GroupID)
+	local Groups = GroupService:GetGroupsAsync(Player)
+	for Index = 1, #Groups do
+		if Groups[Index].Id == GroupID then
 			return true
 		end
 	end
 	return false
 end
 
-function AdvancedGroupService:IsPrimaryGroupAsync(userId, groupId)
-	local Groups = AdvancedGroupService:GetGroupsAsync(userId)
-	for _,group in pairs(Groups) do
-		if (group.IsPrimary == true) then
+function GroupService:IsPrimaryGroupAsync(Player)
+	local Groups = GroupService:GetGroupsAsync(Player)
+	for Index = 1, #Groups do
+		if Groups[Index].IsPrimary then
 			return true
 		end
 	end
 	return false
 end
 
-function AdvancedGroupService:IsInClanAsync(userId, groupId)
-	local Groups = AdvancedGroupService:GetGroupsAsync(userId)
-	for _,group in pairs(Groups) do
-		if (group.IsInClan == true) then
+function GroupService:IsInClanAsync(Player)
+	local Groups = GroupService:GetGroupsAsync(Player)
+	for Index = 1, #Groups do
+		if Groups[Index].IsInClan then
 			return true
 		end
 	end
 	return false
 end
 
-function AdvancedGroupService:IsGroupEnemy(groupId, targetGroupId)
-	local GroupEnemies = AdvancedGroupService:GetGroupEnemiesAsync(groupId)
-	for _, group in pairs(GroupEnemies) do
-		if (group.Id == targetGroupId) then
+function GroupService:IsGroupAlly(GroupID, TargetGroupID)
+	local Groups = GroupService:GetGroupAlliesAsync(GroupID)
+	for Index = 1, #Groups do
+		if Groups[Index].Id == TargetGroupID then
 			return true
 		end
 	end
 	return false
 end
 
-function AdvancedGroupService:IsGroupAlly(groupId, targetGroupId)
-	local GroupEnemies = AdvancedGroupService:GetGroupAlliesAsync(groupId)
-	for _, group in pairs(GroupEnemies) do
-		if (group.Id == targetGroupId) then
+function GroupService:IsGroupEnemy(GroupID, TargetGroupID)
+	local Groups = GroupService:GetGroupEnemiesAsync(GroupID)
+	for Index = 1, #Groups do
+		if Groups[Index].Id == TargetGroupID then
 			return true
 		end
 	end
 	return false
 end
 
-function AdvancedGroupService:GetGroupAlliesAsync(groupId)
-	local GroupAlliesPages = AdvancedGroupService:GetAlliesAsync(groupId)
-	local GroupAllies = AdvancedGroupService:__INTERNALS__():PagesToArray(GroupAlliesPages)
+function GroupService:GetGroupAlliesAsync(GroupID)
+	local Success, GroupAlliesPages = pcall(RbxGroupService.GetAlliesAsync, RbxGroupService, GroupID)
+	local GroupAllies = Success and GroupAlliesPages and PagesToArray(GroupAlliesPages)
 	return GroupAllies
 end
 
-function AdvancedGroupService:GetGroupEnemiesAsync(groupId)
-	local GroupEnemiesPages = AdvancedGroupService:GetEnemiesAsync(groupId)
-	local GroupEnemies = AdvancedGroupService:__INTERNALS__():PagesToArray(GroupEnemiesPages)
+function GroupService:GetEnemiesAsync(GroupID)
+	local Success, GroupEnemiesPages = pcall(RbxGroupService.GetEnemiesAsync, RbxGroupService, GroupID)
+	local GroupEnemies = Success and GroupEnemiesPages and PagesToArray(GroupEnemiesPages)
 	return GroupEnemies
 end
 
---[[ GroupService Functions ]]--
-function AdvancedGroupService:GetAlliesAsync(groupId)
-	return GroupService:GetAlliesAsync(groupId)
+function GroupService:GetGroupsAsync(Player)
+	local Success, Groups = pcall(RbxGroupService.GetGroupsAsync, RbxGroupService, Player.UserId)
+	return Success and Groups or { }
 end
 
-function AdvancedGroupService:GetEnemiesAsync(groupId)
-	return GroupService:GetEnemiesAsync(groupId)
+function GroupService:GetGroupInfoAsync(GroupID)
+	local Success, GroupInfo = pcall(RbxGroupService.GetGroupInfoAsync, RbxGroupService, GroupID)
+	return Success and GroupInfo or { }
 end
 
-function AdvancedGroupService:GetGroupInfoAsync(groupId)
-	return GroupService:GetGroupInfoAsync(groupId)
-end
-
-function AdvancedGroupService:GetGroupsAsync(userId)
-	return GroupService:GetGroupsAsync(userId)
-end
-
-return AdvancedGroupService
+return GroupService
